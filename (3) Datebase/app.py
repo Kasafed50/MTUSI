bffd03414e9fcdedd00c7a80f306291359c40363
Kasafed50 <Kasafed50@gmail.com>
2021-12-06 18:23:55 +0300

    V-1

--- (3) Datebase/app.py
+++ (3) Datebase/app.py
@@ -1,14 +1,34 @@
-from flask import Flask, render_template
+import psycopg2
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+conn = psycopg2.connect(database="service_db",
+                        user="postgres",
+                        password="21root12",
+                        host="localhost",
+                        port="5432")
 
-@app.route('/login/', methods=['GET'])
-def index():
+cursor = conn.cursor()
+
+
+@app.route('/login/', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        if request.form.get("login"):
+            username = request.form.get('username')
+            password = request.form.get('password')
+
+            cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s;", (username, password))
+            if cursor.fetchone() is None:
+                return render_template('login.html', wrong='Wrong login or password')
+
+            cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s;", (username, password))
+            records = list(cursor.fetchone())
+            return render_template("account.html", user=records)
+
     return render_template('login.html')
 
-
-print('http://localhost:5000/login/')
 
 if __name__ == '__main__':
     app.run()
